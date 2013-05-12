3b0b455ff07279d2b3d74640cf61d885f95b929f
shotaro.akiyama <satetsu888@gmail.com>
2013-05-12 12:47:32 +0900

    fix tmpl

--- app.pl
+++ app.pl
@@ -90,43 +90,80 @@ __DATA__
 <html>
 <head>
 <meta charset="utf-8">
-<title>キラキラ暗号</title>
+<meta name="viewport" content="width=device-width,  initial-scale=1">
+<title>*:.。.:*゜キラキラ暗号゜*:.。.:*</title>
 <link href="http://code.jquery.com/mobile/1.0/jquery.mobile-1.0.min.css" rel="stylesheet" type="text/css"/>
+<style type="text/css">
+#page div .ui-grid-a ul li {
+	font-size: xx-small;
+}
+#page div .ui-grid-a h6 {
+	margin-bottom: 0px;
+}
+#page div .ui-grid-a ul {
+	margin-top: 5px;
+}
+</style>
 <script src="http://code.jquery.com/jquery-1.6.4.min.js" type="text/javascript"></script>
+<script type="text/javascript">
+  $(document).bind("mobileinit", function(){
+    $.mobile.ajaxFormsEnabled = false;
+    $.mobile.ajaxEnabled = false;
+  });
+</script>
 <script src="http://code.jquery.com/mobile/1.0/jquery.mobile-1.0.min.js" type="text/javascript"></script>
+<script language="JavaScript" type="text/JavaScript">
+function CopyText(arg){
+    var obj=document.getElementsByName(arg)[0];
+    obj.selectionStart=0;
+	obj.selectionEnd=obj.value.length;
+    document.execCommand("copy");
+}
+//-->
+</script>
 </head> 
 <body> 
 
-<div data-role="page" id="page">
-<div data-role="header">
-		<h1>キラキラ暗号</h1>
+<div data-role="page" id="page" data-theme="c">
+	<div data-role="header" align="center">
+		<img src="http://satetsu888.com/kirakira/logo.png" width="225" height="79">
 	</div>
 	<div data-role="content">
-    <p>キラキラ暗号は日本語の文章をキラキラした記号へと暗号化するサービスです</p>
-    <p>たとえば、「こんにちわ」は「☆.・∮・｡o・｡゜оﾟ∵*+★∮・☆*☆ﾟ¨+∮*о∵∵゜ﾟﾟ」のように変換されます
-    <div class="ui-grid-a">
-    <form method="POST" action="./encode">
-      <div class="ui-block-a">
-      
-        <textarea cols="40" rows="8" name="word" placeholder="暗号化したい文章をいれてね"><%= $word %></textarea>
+        <p>キラキラ暗号は日本語の文章をキラキラした記号へと暗号化するサービスです。<br>たとえば、「こんにちわ」は「☆.・∮・｡o・｡゜оﾟ∵*+★∮・☆*☆ﾟ¨+∮*о∵∵゜ﾟﾟ」のように変換されます。<br>暗号はこのサイトでだけ解くことができます。</p>
+	  <div class="ui-grid-a">
+        <form method="POST" action="./encode">
+	    <div class="ui-block">
+          <input type="text" name="word" value="<%= $word %>" placeholder="暗号化したい文章をいれてね" />
+        </div>
+	    <div class="ui-block">
+	      <input type="submit" value="暗号化する.｡.*:+☆" data-icon="check" />
+        </div>
+        </form>
       </div>
-      <div class="ui-block-b">
-        <input type="submit" value="暗号化する" data-icon="arrow-d" />
+      <div class="ui-grid-a">
+        <form method="POST" action="./decode">
+	    <div class="ui-block">
+          <input type="text" name="kirakira" value="<%= $kirakira %>" placeholder="暗号を解きたい記号をいれてね"/>
+        </div>
+        <div class="ui-block">
+          <input type="button" name="Copy" value="全選択.:*゜:。:." onClick="CopyText('kirakira');">
+        </div>
+	    <div class="ui-block">
+          <input type="submit" value="暗号を解く*:･'ﾟ☆" data-icon="check" />
+	    </div>
+        </form>
       </div>
-      </form>
-      <form method="POST" action="./decode">
-      <div class="ui-block-a">
-        <textarea cols="40" rows="8" name="kirakira" placeholder="暗号を解きたい記号を入れてね"><%= $kirakira %></textarea>
+      <div class="ui-grid-a">
+        <h6>利用上の注意</h6>
+        <ul>
+          <li>パスワードなど大切な情報の暗号化に利用しないでください。</li>
+          <li>暗号化したデータが必ず復号できることを保障しません。</li>
+          <li>予告なくサービスを停止することがありますのでご了承ください。</li>
+        </ul>
       </div>
-      <div class="ui-block-b">
-        <input type="submit" value="暗号を解く" data-icon="arrow-u" />
-      </div>
-      </form>
     </div>
-<div data-role="fieldcontain">
-
-<div data-role="footer">
-    <h6></h6>
+	<div data-role="footer">
+		<h4></h4>
 	</div>
 </div>
 </body>
