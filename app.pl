d7d31e4f5e9cfe9234f92863c17065c00ca45d83
shotaro.akiyama <satetsu888@gmail.com>
2014-04-27 10:47:48 +0900

    Add GoogleAnalytics

--- app.pl
+++ app.pl
@@ -121,6 +121,16 @@ function CopyText(arg){
 }
 //-->
 </script>
+<script>
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-50183657-2', 'kirakira-ango.com');
+ga('send', 'pageview');
+
+</script>
 </head> 
 <body>
 
