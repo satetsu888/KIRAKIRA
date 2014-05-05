ad3b32483c60ced44a04fc202d3ef88d870d83df
shotaro.akiyama <satetsu888@gmail.com>
2014-05-05 16:50:16 +0900

    Implement Throttle Module

--- lib/Kirakira/Throttle.pm
+++ lib/Kirakira/Throttle.pm
@@ -3,15 +3,37 @@ use strict;
 use warnings;
 
 use parent 'Class::Accessor::Fast';
-__PACKAGE__->mk_accessors(qw/ cache /);
+__PACKAGE__->mk_accessors(qw/ cache ip /);
 
 use Cache::Memcached::Fast;
 
 use Kirakira::Config;
 
+my $THROTTLE = [
+    {
+        type   => 'short',
+        expire => 10,
+        count  => 5,
+    },
+    {
+        type   => 'hour',
+        expire => 60 * 60 * 1,
+        count  => 30,
+    },
+    {
+        type   => 'harf-day',
+        expire => 60 * 60 * 12,
+        count  => 60,
+    },
+    {
+        type   => 'day',
+        expire => 60 * 60 * 12,
+        count  => 100,
+    },
+];
+
 sub new {
     my $class = shift;
-
     my $args = ref $_[0] eq 'HASH' ? $_[0] : {@_};
 
     my $config = Kirakira::Config->new();
@@ -20,8 +42,43 @@ sub new {
         cache => Cache::Memcached::Fast->new({
             servers => [ $config->cache ],
         }),
+        ip    => $ENV{'REMOTE_ADDR'},
         %$args,
     });
+}
+
+sub get_cache_key {
+    my $self = shift;
+    my $type = shift;
+
+    return "kirakira:throttle:$self->{ip}:$type";
+}
+
+sub call {
+    my $self = shift;
+
+    for my $pattern ( @$THROTTLE ){
+        my $cache_key = $self->get_cache_key($pattern->{type});
+        my $count = $self->cache->get($cache_key);
+        if($count){
+            $self->cache->incr($cache_key, 1);
+        } else {
+            $self->cache->set($cache_key, 1, $pattern->{expire});
+        }
+    }
+}
+
+sub is_restricted {
+    my $self = shift;
+
+    for my $pattern ( @$THROTTLE ){
+        my $cache_key = $self->get_cache_key($pattern->{type});
+
+        my $count = $self->cache->get($cache_key);
+        return 1 if $count && $count > $pattern->{count};
+    }
+
+    return 0;
 }
 
 1;
--- t/lib/Kirakira/Throttle.t
+++ t/lib/Kirakira/Throttle.t
@@ -13,5 +13,38 @@ subtest basic => sub {
     isa_ok($throttle->cache, 'Cache::Memcached::Fast');
 };
 
+subtest is_restricted => sub {
+    my $throttle = Kirakira::Throttle->new();
+    my $dummy_ip = 'dummy_for_is_restricted_test'. int rand 0xFFFF;
+    $throttle->ip($dummy_ip);
+
+    $throttle->cache->set(
+        "kirakira:throttle:$dummy_ip:short",
+        10,
+    );
+
+    is($throttle->is_ristricted(), 1);
+};
+
+subtest call => sub {
+    my $throttle = Kirakira::Throttle->new();
+    my $dummy_ip = 'dummy_for_call_test'. int rand 0xFFFF;
+    $throttle->ip($dummy_ip);
+
+    $throttle->call();
+    is($throttle->is_ristricted(), 0);
+    $throttle->call();
+    is($throttle->is_ristricted(), 0);
+    $throttle->call();
+    is($throttle->is_ristricted(), 0);
+    $throttle->call();
+    is($throttle->is_ristricted(), 0);
+    $throttle->call();
+    is($throttle->is_ristricted(), 0);
+    $throttle->call();
+    is($throttle->is_ristricted(), 1, 'get restricted call 6th time in short duration');
+
+};
+
 done_testing;
 
